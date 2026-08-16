2294fb5d57b7a75b6ade138bb7edb99d9f9d0346
Stojan Anastasov <stolea@gmail.com>
2026-08-16 12:44:56 +0200

    Use the correct retain function

--- app/src/main/java/dev/sanastasov/bybon/onermcalc/OneRmCalculatorScreen.kt
+++ app/src/main/java/dev/sanastasov/bybon/onermcalc/OneRmCalculatorScreen.kt
@@ -21,7 +21,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.retain.retain
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -31,6 +30,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import dev.marcellogalhardo.retained.compose.retain
 
 @Composable
 fun OneRmCalculatorScreen() {
