ce63a6eb00f8d1a998cd5b1e5bd1cd7fcbeb9336
Stojan Anastasov <stolea@gmail.com>
2026-09-06 10:43:09 +0200

    [Workouts] Start workout button

--- app/src/main/java/dev/sanastasov/bybon/workout/ui/plans/WorkoutPlansScreen.kt
+++ app/src/main/java/dev/sanastasov/bybon/workout/ui/plans/WorkoutPlansScreen.kt
@@ -11,11 +11,14 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import dev.sanastasov.bybon.workout.domain.WorkoutPlan
 import dev.sanastasov.bybon.workout.domain.fullBodyA
 import dev.sanastasov.bybon.workout.domain.fullBodyB
@@ -32,25 +35,37 @@ fun WorkoutsTab(plans: List<WorkoutPlan>, modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.spacedBy(16.dp),
     ) {
         items(plans) { plan ->
-            WorkoutPlanCard(plan)
+            WorkoutPlanCard(plan, {})
         }
     }
 }
 
 @Composable
-private fun WorkoutPlanCard(plan: WorkoutPlan) {
+private fun WorkoutPlanCard(plan: WorkoutPlan, onStartWorkoutClicked: (WorkoutPlan) -> Unit) {
     Card(Modifier.fillMaxWidth()) {
-        Column(Modifier.padding(8.dp), Arrangement.spacedBy(4.dp)) {
+        Column(
+            Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            Arrangement.spacedBy(4.dp)
+        ) {
             Text(plan.name, fontWeight = FontWeight.Bold)
             Spacer(Modifier.height(4.dp))
 
             plan.description?.let {
-                Text(it)
+                Text(it, fontSize = 14.sp)
             }
 
             plan.setsByExercise.forEach { (exerciseId, sets) ->
                 val exerciseSet = sets.first()
                 Text("${sets.size} x ${exerciseSet.exercise.name} for ${exerciseSet.repRange.first} to ${exerciseSet.repRange.last}")
+            }
+
+            TextButton(
+                { onStartWorkoutClicked(plan) },
+                Modifier.align(Alignment.CenterHorizontally)
+            ) {
+                Text("Start Workout")
             }
         }
     }
